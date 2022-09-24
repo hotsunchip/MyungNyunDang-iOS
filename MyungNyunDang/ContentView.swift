ecde7d98d5b65a7dca657619acfa7127be1f1962
hotsunchip <hotsunchip@kaist.ac.kr>
2022-09-25 03:16:54 +0900

    Initial Commit

---
+++ MyungNyunDang/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyungNyunDang
+//
+//  Created by PLUME.D on 2022/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyungNyunDang/MyungNyunDangApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyungNyunDangApp.swift
+//  MyungNyunDang
+//
+//  Created by PLUME.D on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MyungNyunDangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
